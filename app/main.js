6b27feaa40f353e07556c8c3628701fb15419229
Wellington2708 <wellingtondevelopercontact@gmail.com>
2023-02-28 21:11:51 +0000

    trabalhando metodos forEach, Map e Filter

--- app/main.js
+++ app/main.js
@@ -3,31 +3,12 @@
 let livros = []  //para fazer uma array
 const requisicao = 'https://guilhermeonrails.github.io/casadocodigo/livros.json'   //para pegar os objetos JS.
 getBuscarLivrosDaAPI() //chamando função assincrona. 
-const elementoParaInserirLivros = document.getElementById('livros')  //criar constante buscando o elemento no HTML
 
 //criando função assincrona 
 async function getBuscarLivrosDaAPI() {
     const res = await fetch(requisicao)
     livros = await res.json()
-    console.table(livros)
-    exibirOsLivrosNaTela(livros)
+    let  livrosComDesconto = aplicarDesconto(livros);
+    exibirOsLivrosNaTela(livrosComDesconto);
 }
 
-function exibirOsLivrosNaTela(listaDeLivros) {
-    listaDeLivros.forEach(livro => {
-        elementoParaInserirLivros.innerHTML += `
-        <div class="livro">
-        <img class="livro__imagens" src="${livro.imagem}"
-          alt="${livro.alt}" />
-        <h2 class="livro__titulo">
-          ${livro.titulo}
-        </h2>
-        <p class="livro__descricao">${livro.autor}</p>
-        <p class="livro__preco" id="preco">R$${livro.preco}</p>
-        <div class="tags">
-          <span class="tag">${livro.categoria}</span>
-        </div>
-      </div>
-        `
-    })
-}
